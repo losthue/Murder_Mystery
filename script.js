f43dab45f3974517b115a0a6c0392196c7e13076
Nishika-S <seenundunsia@gmail.com>
2026-03-30 01:21:06 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function checkBinary() {
   let input = document.getElementById("answer").value.toLowerCase();
 
-  if (input === "rafi") {
+  if (input === "book") {
     document.getElementById("result").innerHTML =
       "🔓 Folder unlocked.<br><br>" +
       "<em>'If you're reading this, I'm already gone. It was the coffee. I felt it immediately. I know who did it. I should have trusted my instincts about Rafi years ago. The book was the beginning. The poison was the end. The gold is where I first—'</em>" +
